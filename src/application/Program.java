4368a1cd55e6ac04c4087a49cd8e7a58746d1cb6
Bowie <marketing@letscode.com.br>
2025-01-22 23:25:16 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,5 +33,10 @@ public class Program {
         for (Seller obj : list2) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller seller2 = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(seller2);
+        System.out.println("Inserted! New id = " + seller2.getId());
     }
 }
